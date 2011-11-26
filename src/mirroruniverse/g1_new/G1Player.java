451fc873f54b9400c973295d0981bbbfa00f8693
Ashish <ashish.s.chhabria@gmail.com>
2011-11-26 15:49:56 -0500

    Code is integrated.
    However, the A* is never called -  because one player steps on the exit before the other player can see it.

--- src/mirroruniverse/g1_new/G1Player.java
+++ src/mirroruniverse/g1_new/G1Player.java
@@ -1,8 +1,10 @@
 package mirroruniverse.g1_new;
 
+import java.util.LinkedList;
 import java.util.Random;
 
-import mirroruniverse.g2.Config;
+import mirroruniverse.g1_new.Info;
+import mirroruniverse.g1_new.Node;
 import mirroruniverse.sim.MUMap;
 import mirroruniverse.sim.Player;
 
@@ -11,15 +13,89 @@ public class G1Player implements Player {
 	static boolean initialized = false;
 	static Exploration explore = new Exploration();
 	
+
+	boolean blnLOver = false;
+	boolean blnROver = false;
+	int lastXMove;
+	int lastYMove;
+	static boolean seeLeftExit = false;
+	static boolean seeRightExit = false;
+	int[] directionL;
+	int[] directionR;
+	LinkedList<Node> pathL;
+	LinkedList<Node> pathR;
+	int nextMoveL;
+	int nextMoveR;
+	boolean firstMove = true;
+	static int directionForThisRound = 0, directionForPreviousRound = 0;
+	static boolean updateLeft = false;
+	static boolean updateRight = false;
+	
 	@Override
 	public int lookAndMove(int[][] aintViewL, int[][] aintViewR) {
 		// TODO Auto-generated method stub
+		/*
+		 * Initialize the player.
+		 */
 		if (!initialized){
 			Info.initInfo(aintViewL.length, aintViewR.length);
 			initialized = true;
+			System.out.println("Called INITIALIZED ONCE ONLY");
 		}
+		/*
+		 * If Left player moved 
+		 */
+		if (updateLeft){
+		Info.updateGlobalLocation('l', aintViewL,directionForPreviousRound);
+		updateLeft = false;
+		}
+		/*
+		 * If Right player moved
+		 */
+		if (updateRight){
+		Info.updateGlobalLocation('r', aintViewR, directionForPreviousRound);
+		updateRight = false;
+		}
+
+		if (Info.scanMap(Info.aintGlobalViewL, MapData.exit) != null) seeLeftExit = true;
+		if (Info.scanMap(Info.aintGlobalViewR, MapData.exit) != null) seeRightExit= true;
+		/*
+		 * If both exits are visible, use A* to get
+		 * the next direction
+		 */
+		if (seeLeftExit && seeRightExit){
+			System.out.println("Time for A*");
+			pathL = Info.aStar3(Info.aintGlobalViewL);
+			directionL = Info.directionToMove(pathL, Info.aintGlobalViewL);
+
+			pathR = Info.aStar3(Info.aintGlobalViewR);
+			directionR = Info.directionToMove(pathR, Info.aintGlobalViewR);
+			
+			if(!blnLOver) {
+				nextMoveL++;
+				directionForThisRound = directionL[nextMoveL];
+			}
+			
+			else if(!blnROver) {
+				nextMoveR++;
+				directionForThisRound = directionR[nextMoveR];
+			}	
+		}
+		/*
+		 * Else move randomly, and explore
+		 */
+		directionForThisRound = explore.randomMove();
 		
-		return explore.randomMove();
+		if ( aintViewL[aintViewL.length / 2 + MUMap.aintDToM[directionForThisRound][0]][aintViewL.length / 2 + MUMap.aintDToM[directionForThisRound][1]] != 1){
+			updateLeft = true;
+			System.out.println("We need to update Left position");
+		}
+		if ( aintViewR[aintViewR.length / 2 + MUMap.aintDToM[directionForThisRound][0]][aintViewR.length / 2 + MUMap.aintDToM[directionForThisRound][1]] != 1){
+			updateRight = true;
+			System.out.println("We need to update Right position");
+		}
+		directionForPreviousRound = directionForThisRound;
+		return directionForThisRound;
 	}
 
 }
--- src/mirroruniverse/g1_new/Info.java
+++ src/mirroruniverse/g1_new/Info.java
@@ -1,5 +1,11 @@
 package mirroruniverse.g1_new;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+import mirroruniverse.g1_new.Node;
 import mirroruniverse.g1_new.MapData;
 import mirroruniverse.sim.MUMap;
 
@@ -19,6 +25,16 @@ public class Info {
 	static int aintGlobalViewR [][];
 	
 	static int currLX, currLY, currRX, currRY;
+	
+
+	static ArrayList<LinkedList<Node>> path = new ArrayList<LinkedList<Node>>();
+	static ArrayList<Node> searchGraph = new ArrayList<Node>();
+	static ArrayList<Node> open = new ArrayList<Node>();
+	static ArrayList<Node> closed = new ArrayList<Node>();
+	static ArrayList <Node> m = new ArrayList<Node>();
+	static ArrayList <Node> came_from = new ArrayList<Node>();
+	public static final int[][] aintDToM = { { 0, 0 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 },  { -1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 } };
+	
 	
 	public static void initInfo ( int visibilityRadiusL, int visibilityRadiusR){
 		astar = new Astar();
@@ -44,12 +60,12 @@ public class Info {
 		int intDeltaY = aintMove[ 1 ];
 		
 		System.out.println("Okay, now the Delta Y and X are " + intDeltaY + " " + intDeltaX);
-		
 		if (side == 'l'){
 			System.out.println("Updating Relative Location for Left side");
 			currLX += intDeltaX;
 			currLY += intDeltaY;
 		}
+		
 		else if (side == 'r'){
 			System.out.println("Updating Relative Location for Right side");
 			currRX += intDeltaX;
@@ -73,13 +89,14 @@ public class Info {
 			System.out.println("Updating Global view for left player");
 			for (int i=0; i<view.length; i++)
 				for (int j=0; j<view[i].length; j++)
-					aintGlobalViewL[i+100+currLY][j+100+currLX] = view [i][j];
+					aintGlobalViewL[i+99+currLY][j+99+currLX] = view [i][j];
 		}
 		else if (side == 'r'){
 			System.out.println("Updating Global view for right player");
 			for (int i=0; i<view.length; i++)
 				for (int j=0; j<view[i].length; j++)
-					aintGlobalViewR[i+100+currRY][j+100+currRX] = view [i][j];
+					aintGlobalViewR[i+99+currRY][j+99+currRX] = view [i][j];
+					
 		}
 		System.out.println("Global View for side " + side + "has been updated");
 	}
@@ -101,5 +118,140 @@ public class Info {
 			aintLocalViewL = tempLocalView;
 		}
 	}
+	
+	public static int[] directionToMove(LinkedList<Node> path, int[][] globalView) {
+		int [] direction = new int[path.size()-1];
+		Node currPosn = scanMap(globalView, MapData.playerPosition);
+		
+		int k = 0;
+		
+		for(int i = 1 ; i <= path.size() ; i++ ) {
+			for( int x= -1; x <= 1 ; x++ )
+				for( int y= -1; y <= 1 ; y++ )
+					if(new Node(currPosn.getX() + x , currPosn.getY() + y ) == path.get(i)) {
+						for(int j = 0 ; j < aintDToM.length; i++)
+							if(x == aintDToM[j][0] && y == aintDToM[j][1]) {
+								direction [k] = j;
+								k++;
+							} 
+					}
+						
+			}
+
+			return direction;
+		}
+	
+	public static LinkedList<Node> aStar3 (int [][] globalView ){
+
+		Node start = scanMap(globalView, MapData.playerPosition);
+		Node exit = scanMap(globalView, MapData.exit);
+
+		open.add(start);
+
+		ArrayList<Node> succesors = new ArrayList<Node>();
+		LinkedList<Node> path = new LinkedList<Node>();
+		Map<Node, Double> tentCost = new HashMap<Node, Double>();
+
+		path.add(start);
+
+		do{
+			double min = Double.MAX_VALUE;
+			Node minCostNode = new Node(start.getX(), start.getY());
+			Node tempMin = new Node(start.getX(), start.getY());
+
+			/*
+			 * Looking for the node in open set having the least
+			 * fvalue.
+			 */
+			for(Node m : open){
+				if(min > (gfunc(m, exit)+ hfunc(m,exit,globalView))){
+					minCostNode = m;
+					min = gfunc(m, exit)+ hfunc(m,exit,globalView);
+					}
+			}
+				open.remove(minCostNode);
+				closed.add(minCostNode);
+				tempMin = minCostNode;
+
+
+			boolean isBetter = true;
+
+			if(tempMin == exit)
+				return path;
+
+			else {
+				succesors = generateSuccessors(tempMin);
+
+				for(Node m : succesors) {
+					if(closed.contains(m))	continue;
+
+					if(!open.contains(m)) {
+						open.add(m);
+						tentCost.put(m, (gfunc(tempMin,exit)+ gfunc(tempMin,m)));
+					}
+
+					if(tentCost.get(m) < gfunc(m,exit)) {
+						isBetter = true;
+					}
+					else isBetter = false;
+
+					if(isBetter) {
+						path.add(m);
+						double f = tentCost.get(m) + hfunc(m, exit, globalView); 
+						tentCost.remove(m);
+						tentCost.put(m, f);
+					}
+
+				}
+			}
+
+		}while(!open.isEmpty());
+
+		return path;
+	}
+	
+
+
+	public static double hfunc (Node start, Node end, int globalView[][]){
+		Double hValue = (double) (Math.abs(start.getX()-end.getX())+ Math.abs(start.getY()-end.getY()));
+		if(globalView[start.getX()][start.getY()] == 1)
+			hValue += 1000;
+		return hValue;
+	}
+	public static double gfunc(Node start, Node end){
+		Double gValue = (double) (Math.abs(start.getX()-end.getX())+ Math.abs(start.getY()-end.getY()));
+		return gValue;
+	}
+	public static ArrayList<Node> generateSuccessors ( Node n){
+		ArrayList<Node> successors = new ArrayList<Node>();
+
+		int i=0,j=0;
+		for (i =-1 ; i <= 1; i++ )
+			for ( j=-1 ; j<=1 ;j++){
+				Node temp = new Node(n.getX()+i, n.getY()+j);
+				if (n != temp)
+					successors.add(temp);
+			}
+		return successors;
+	}
+	
+	public static Node scanMap (int view[][], int x){
+		int i=0,j=0;
+		Node ret = new Node();
+		for (i= 0;i<view.length ; i++)
+			for (j=0 ; j<view[i].length ; j++)
+				{
+					if (view [i][j] == x){
+						ret.setX(i);
+						ret.setY(j);
+						return ret;
+					}
+					else
+						continue;
+				}
+
+		return null;
+	}
+
 }
 
